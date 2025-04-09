33eabbc65516807d66a59005542e03a4551b761b
root <root@ip-172-31-7-172.ap-south-1.compute.internal>
2025-04-09 11:49:16 +0000

    updated the syntax

--- app.py
+++ app.py
@@ -2,4 +2,4 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 sum = num1 + num2
-print("The sum of", num1, "and", num2, "is:", sum)
+print("The sum of", num1, "and", num2, "is:", diff)
