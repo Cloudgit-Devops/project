a8ae4ae4f10979e2ceeb1a7728c679e19847cc26
ayesha <ayeshask421@gmail.com>
2025-04-09 17:14:50 +0530

    modified the sum into diff

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Simple Python Program to add two numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-sum = num1 + num2
+diff = num1 - num2
 print("The sum of", num1, "and", num2, "is:", sum)
