5beeb723a23f307240457de4770520d50ee5023b
ayesha <ayeshask421@gmail.com>
2025-04-10 14:22:20 +0530

    modified app.py in my branch

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 # Simple Python Program to add two numbers
-<<<<<<< HEAD
 num1 = float(input("Enter the first number as: "))
 num2 = float(input("Enter the second number: "))
-=======
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number as: "))
->>>>>>> 0107f44 (change the data)
-<<<<<<< HEAD
+
 sum = num1 + num2
-print("The sum ", num1, "and", num2, "is:", diff)
-=======
+print("The sum ", num1, "and", num2, "is:", sum
 diff = num1 - num2
-print("The sum of", num1, "and", num2, "is:", sum)
->>>>>>> a8ae4ae4f10979e2ceeb1a7728c679e19847cc26
+print("The sum of", num1, "and", num2, "is:", diff)
+
